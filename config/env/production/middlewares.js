ec4f53fa172bcba2163ed3c8c6dc46221a81d9c3
Paul Bratslavsky <paulbratslavsky@Pauls-MacBook-Pro.local>
2024-06-15 04:04:35 -0500

    initial commit

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,28 +1,9 @@
 module.exports = [
+  'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
-          'media-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'res.cloudinary.com',
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
-  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
