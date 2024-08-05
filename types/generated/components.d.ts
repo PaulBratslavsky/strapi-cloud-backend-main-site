56f75c46a91f53c14c06c191dcc5da083dc33f52
Paul Bratslavsky <paulbratslavsky@Pauls-MacBook-Pro.local>
2024-08-05 14:37:54 -0700

    migrat to strapi 5

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,71 +1,67 @@
-import type { Schema, Attribute } from '@strapi/strapi';
+import type { Struct, Schema } from '@strapi/strapi';
 
-export interface LayoutVideo extends Schema.Component {
+export interface LayoutVideo extends Struct.ComponentSchema {
   collectionName: 'components_layout_videos';
   info: {
     displayName: 'Video';
   };
   attributes: {
-    title: Attribute.String;
-    description: Attribute.String;
-    videoId: Attribute.String;
-    videoUrl: Attribute.String;
-    clip: Attribute.Component<'elements.video-clip', true>;
+    title: Schema.Attribute.String;
+    description: Schema.Attribute.String;
+    videoId: Schema.Attribute.String;
+    videoUrl: Schema.Attribute.String;
+    clip: Schema.Attribute.Component<'elements.video-clip', true>;
   };
 }
 
-export interface LayoutTopNav extends Schema.Component {
+export interface LayoutTopNav extends Struct.ComponentSchema {
   collectionName: 'components_layout_top_navs';
   info: {
     displayName: 'TopNav';
     description: '';
   };
   attributes: {
-    logoLink: Attribute.Component<'elements.link'>;
-    navItem: Attribute.Component<'elements.link', true>;
+    logoLink: Schema.Attribute.Component<'elements.link', false>;
+    navItem: Schema.Attribute.Component<'elements.link', true>;
   };
 }
 
-export interface LayoutPostList extends Schema.Component {
+export interface LayoutPostList extends Struct.ComponentSchema {
   collectionName: 'components_layout_post_lists';
   info: {
     displayName: 'Post List';
   };
   attributes: {
-    title: Attribute.String;
-    description: Attribute.String;
-    posts: Attribute.Relation<
-      'layout.post-list',
-      'oneToMany',
-      'api::post.post'
-    >;
+    title: Schema.Attribute.String;
+    description: Schema.Attribute.String;
+    posts: Schema.Attribute.Relation<'oneToMany', 'api::post.post'>;
   };
 }
 
-export interface LayoutHero extends Schema.Component {
+export interface LayoutHero extends Struct.ComponentSchema {
   collectionName: 'components_layout_heroes';
   info: {
     displayName: 'Hero';
   };
   attributes: {
-    heading: Attribute.String;
-    text: Attribute.Text;
-    image: Attribute.Media<'images'>;
-    buttonLink: Attribute.Component<'elements.button-link', true>;
+    heading: Schema.Attribute.String;
+    text: Schema.Attribute.Text;
+    image: Schema.Attribute.Media<'images'>;
+    buttonLink: Schema.Attribute.Component<'elements.button-link', true>;
   };
 }
 
-export interface LayoutCode extends Schema.Component {
+export interface LayoutCode extends Struct.ComponentSchema {
   collectionName: 'components_layout_codes';
   info: {
     displayName: 'Code';
     description: '';
   };
   attributes: {
-    title: Attribute.String;
-    description: Attribute.Text;
-    code: Attribute.Text;
-    language: Attribute.Enumeration<
+    title: Schema.Attribute.String;
+    description: Schema.Attribute.Text;
+    code: Schema.Attribute.Text;
+    language: Schema.Attribute.Enumeration<
       [
         'javascript',
         'typescript',
@@ -84,54 +80,54 @@ export interface LayoutCode extends Schema.Component {
         'javascriptreact',
         'typescriptreact',
         'jsonc',
-        'c'
+        'c',
       ]
     >;
   };
 }
 
-export interface ElementsVideoClip extends Schema.Component {
+export interface ElementsVideoClip extends Struct.ComponentSchema {
   collectionName: 'components_elements_video_clips';
   info: {
     displayName: 'Video Clip';
   };
   attributes: {
-    start: Attribute.Float;
-    end: Attribute.Float;
-    title: Attribute.String;
+    start: Schema.Attribute.Float;
+    end: Schema.Attribute.Float;
+    title: Schema.Attribute.String;
   };
 }
 
-export interface ElementsLink extends Schema.Component {
+export interface ElementsLink extends Struct.ComponentSchema {
   collectionName: 'components_elements_links';
   info: {
     displayName: 'Link';
     description: '';
   };
   attributes: {
-    text: Attribute.String;
-    href: Attribute.String;
-    isExternal: Attribute.Boolean & Attribute.DefaultTo<false>;
+    text: Schema.Attribute.String;
+    href: Schema.Attribute.String;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
   };
 }
 
-export interface ElementsButtonLink extends Schema.Component {
+export interface ElementsButtonLink extends Struct.ComponentSchema {
   collectionName: 'components_elements_button_links';
   info: {
     displayName: 'Button Link';
     description: '';
   };
   attributes: {
-    text: Attribute.String;
-    isExternal: Attribute.Boolean & Attribute.DefaultTo<false>;
-    type: Attribute.Enumeration<['PRIMARY', 'SECONDARY']>;
-    href: Attribute.String;
+    text: Schema.Attribute.String;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    type: Schema.Attribute.Enumeration<['PRIMARY', 'SECONDARY']>;
+    href: Schema.Attribute.String;
   };
 }
 
-declare module '@strapi/types' {
-  export module Shared {
-    export interface Components {
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
       'layout.video': LayoutVideo;
       'layout.top-nav': LayoutTopNav;
       'layout.post-list': LayoutPostList;
