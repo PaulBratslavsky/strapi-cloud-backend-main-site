b0a3d44d603c31aa99ccb62cd5f7200a8edf300f
paul bratslavsky <paulbratslavsky@Mac.localdomain>
2024-11-15 14:02:57 -0600

    fix

--- config/plugins.js
+++ config/plugins.js
@@ -26,16 +26,16 @@ module.exports = ({ env }) => ({
   utilai: {
     enabled: true,
   },
-'yt-transcript': {
-    enabled: true,
-    resolve: './src/plugins/yt-transcript',
-    config: {
-      openAIApiKey: env('OPENAI_API_KEY'),
-      model: env('OPEN_AI_MODEL', 'gpt-4o-mini'),
-      temp: env('OPEN_AI_TEMPERATURE', 0.7),
-      maxTokens: env('OPEN_AI_MAX_TOKENS', 1000),
-    },
-  },
+// 'yt-transcript': {
+//     enabled: true,
+//     resolve: './src/plugins/yt-transcript',
+//     config: {
+//       openAIApiKey: env('OPENAI_API_KEY'),
+//       model: env('OPEN_AI_MODEL', 'gpt-4o-mini'),
+//       temp: env('OPEN_AI_TEMPERATURE', 0.7),
+//       maxTokens: env('OPEN_AI_MAX_TOKENS', 1000),
+//     },
+//   },
   "get-strapi-schema": {
     enabled: true,
   },
