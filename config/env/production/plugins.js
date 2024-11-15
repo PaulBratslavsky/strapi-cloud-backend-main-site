9ff65e4f0426770b4c988967f8779fc9eb368230
paul bratslavsky <paulbratslavsky@Mac.localdomain>
2024-11-14 23:04:38 -0600

    why

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -26,12 +26,11 @@ module.exports = ({ env }) => ({
   },
   'yt-transcript': {
     enabled: true,
-    // resolve: './src/plugins/yt-transcript',
     config: {
       openAIApiKey: env('OPENAI_API_KEY'),
-      model: env('OPEN_AI_MODEL'),
-      temp: env('OPEN_AI_TEMPERATURE'),
-      maxTokens: env('OPEN_AI_MAX_TOKENS'),
+      model: env('OPEN_AI_MODEL', 'gpt-4o-mini'),
+      temp: env('OPEN_AI_TEMPERATURE', 0.7),
+      maxTokens: env('OPEN_AI_MAX_TOKENS', 2000),
     },
   },
 });
